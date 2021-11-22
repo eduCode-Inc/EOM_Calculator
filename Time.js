9ab1c4bda48c11f49dd7323431345ad40b529e3e
tanay-desai <91648918+tanay-desai@users.noreply.github.com>
2021-11-22 13:24:18 +0530

    Completed Time

--- Time.js
+++ Time.js
@@ -1,4 +1,34 @@
 function Time()
-{
-    
+{                    
+        u = prompt("Enter initial velocity(u)");	
+	document.write("Initial Velocity: " + u + "<br>");
+	u = parseFloat(u);
+        a = prompt("Enter acceleration(a)");
+	document.write("Acceleration: " + a + "<br>");
+	a = parseFloat(a);
+        choice = prompt("Enter 'd' if you have Distance and 'v' if you have Final Velocity");        
+        switch (choice)
+        {
+            case 'd':
+            {
+                d = prompt("Enter distance(d)");
+		document.write("Distance: " + d + "<br>");
+		d = parseFloat(d);		                
+                v = Math.sqrt(u*u + 2*a*d);
+                t = (v - u) / a;
+		document.write("Time: " + t + "<br>");
+                break;
+            }
+            case 'v':
+            {
+                t = prompt("Enter FinalVelocity(v)"); 
+		document.write("Final Velocity: " + v + "<br>");
+		t = parseFloat(t);               
+                t = (v - u) / a;
+		document.write("Time: " + t + "<br>");
+                break;
+            }            		
+        }
+        alert("Time = " + t + " Click OK to see summary");
+	document.write("Thank for using eduCode's product. To know more about eduCode, click on the link below" + "<br>");
 }
