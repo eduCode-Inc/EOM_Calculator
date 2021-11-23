95e462ec629868e6103a67ed32496389b207db47
Medhansh <87754879+Medhansh-shah@users.noreply.github.com>
2021-11-23 17:24:59 +0530

    stylised it

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 document.write("<p id = 'beginning'>" + "Welcome to EoM Calculator!" + "<br>" + "Using this, you can calculate any value in the equations of motion!" + "<br>" + "</p>");
-document.write("Enter 'a' for acceleration" + "<br>" + "Enter 'u' for initial velocity" + "<br>" + "Enter 'v' for final velocity" + "<br>" + "Enter 't' for time" + "<br>" + "Enter 'd' for distance" + "<br>");
+document.write("<p id = 'index'>" + "Enter 'a' for acceleration" + "<br>" + "Enter 'u' for initial velocity" + "<br>" + "Enter 'v' for final velocity" + "<br>" + "Enter 't' for time" + "<br>" + "Enter 'd' for distance" + "<br>" + "</p>");
 //document.write("Enter which value would you like to calculate: ");
 function askInput() {
     chzzzz = prompt("Enter character: ");
